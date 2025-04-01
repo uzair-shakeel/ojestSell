0fe15988aa04b12050019f0b6bb5c1fb67a4a3f5
yousri <129407665+yousriB@users.noreply.github.com>
2025-04-01 01:47:52 +0100

    update add  cars

--- app/dashboard/messages/page.jsx
+++ app/dashboard/messages/page.jsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import React, { useState, useEffect, useRef } from "react";
 import { FaSearch, FaPaperPlane, FaBars, FaEnvelope } from "react-icons/fa";
 
@@ -8,31 +8,68 @@ const conversations = [
     name: "Jasmin Trewen",
     messages: [
       { text: "Hello!", sender: "user", time: "9:56 AM" },
-      { text: "Hello, Jasmin Trewen! Welcome to the Champion Store WhatsApp! Let me know how I can help you.", sender: "agent", time: "9:57 AM" },
-      { text: "Nothing.\nI just want to talk to you.", sender: "user", time: "10:07 AM" },
-      { text: "What would you like to talk about? Can I help you?", sender: "agent", time: "10:15 AM" },
-      { text: "I bought a book some time ago, but I lost it. What should I do? Should I buy it again here or just try to find it?", sender: "user", time: "10:28 AM" },
-    ]
+      {
+        text: "Hello, Jasmin Trewen! Welcome to the Champion Store WhatsApp! Let me know how I can help you.",
+        sender: "agent",
+        time: "9:57 AM",
+      },
+      {
+        text: "Nothing.\nI just want to talk to you.",
+        sender: "user",
+        time: "10:07 AM",
+      },
+      {
+        text: "What would you like to talk about? Can I help you?",
+        sender: "agent",
+        time: "10:15 AM",
+      },
+      {
+        text: "I bought a book some time ago, but I lost it. What should I do? Should I buy it again here or just try to find it?",
+        sender: "user",
+        time: "10:28 AM",
+      },
+    ],
   },
   {
     id: 2,
     name: "Jonas Kahnwald",
     messages: [
-      { text: "Hello! Is this the number of the Champion store?", sender: "user", time: "9:56 AM" },
-      { text: "Hello, Jonas Kahnwald! Welcome to the dashboard.", sender: "agent", time: "9:57 AM" },
-      { text: "I want to add a new car to the website.", sender: "user", time: "10:07 AM" },
-      { text: "What would you like to talk about? Can I help you?", sender: "agent", time: "10:15 AM" },
-      { text: "I bought a book  What should I do? Should I buy it again here or just try to find it?", sender: "user", time: "10:28 AM" },
-    ]
-  }
+      {
+        text: "Hello! Is this the number of the Champion store?",
+        sender: "user",
+        time: "9:56 AM",
+      },
+      {
+        text: "Hello, Jonas Kahnwald! Welcome to the dashboard.",
+        sender: "agent",
+        time: "9:57 AM",
+      },
+      {
+        text: "I want to add a new car to the website.",
+        sender: "user",
+        time: "10:07 AM",
+      },
+      {
+        text: "What would you like to talk about? Can I help you?",
+        sender: "agent",
+        time: "10:15 AM",
+      },
+      {
+        text: "I bought a book  What should I do? Should I buy it again here or just try to find it?",
+        sender: "user",
+        time: "10:28 AM",
+      },
+    ],
+  },
 ];
 
 const MessagesPage = () => {
-  const [selectedConversation, setSelectedConversation] = useState(conversations[0]);
+  const [selectedConversation, setSelectedConversation] = useState(
+    conversations[0]
+  );
   const [newMessage, setNewMessage] = useState("");
   const [showSidebar, setShowSidebar] = useState(true);
   const messagesEndRef = useRef(null);
-
 
   useEffect(() => {
     messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -42,7 +79,17 @@ const MessagesPage = () => {
     if (newMessage.trim()) {
       setSelectedConversation((prev) => ({
         ...prev,
-        messages: [...prev.messages, { text: newMessage, sender: "agent", time: new Date().toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) }],
+        messages: [
+          ...prev.messages,
+          {
+            text: newMessage,
+            sender: "agent",
+            time: new Date().toLocaleTimeString([], {
+              hour: "2-digit",
+              minute: "2-digit",
+            }),
+          },
+        ],
       }));
       setNewMessage("");
     }
@@ -50,7 +97,11 @@ const MessagesPage = () => {
 
   return (
     <div className="flex min-h-[570px] h-auto bg-white font-sans relative">
-      <div className={`fixed md:static top-0 z-20 bg-white h-full md:h-auto w-full sm:w-[320px] border-r border-gray-300 flex flex-col transition-transform duration-300 ${showSidebar ? 'translate-x-0' : '-translate-x-full'} md:translate-x-0`}>
+      <div
+        className={`fixed md:static top-0 z-20 bg-white h-full md:h-auto w-full sm:w-[320px] border-r border-gray-300 flex flex-col transition-transform duration-300 ${
+          showSidebar ? "translate-x-0" : "-translate-x-full"
+        } md:translate-x-0`}
+      >
         <div className="flex-1 overflow-auto px-4 space-y-2 py-4">
           {conversations.map((conversation) => (
             <div
@@ -64,7 +115,9 @@ const MessagesPage = () => {
               <div className="w-10 h-10 bg-green-400 rounded-full"></div>
               <div>
                 <div className="font-medium text-sm">{conversation.name}</div>
-                <div className="text-xs text-gray-500 truncate w-40">{conversation.messages[conversation.messages.length - 1].text}</div>
+                <div className="text-xs text-gray-500 truncate w-40">
+                  {conversation.messages[conversation.messages.length - 1].text}
+                </div>
               </div>
             </div>
           ))}
@@ -74,21 +127,38 @@ const MessagesPage = () => {
       <div className="flex-1 flex flex-col">
         <div className="border-b border-gray-300 p-4 flex items-center justify-between">
           <div>
-            <div className="font-medium text-lg">{selectedConversation.name}</div>
+            <div className="font-medium text-lg">
+              {selectedConversation.name}
+            </div>
           </div>
           <button
-  className="md:hidden fixed top-24 right-4 z-10 p-2 px-5 bg-white border rounded-md shadow"
-  onClick={() => setShowSidebar(!showSidebar)}
->
-  <FaEnvelope className="text-blue-500" size={20} />
-</button>
+            className="md:hidden fixed top-24 right-4 z-10 p-2 px-5 bg-white border rounded-md shadow flex items-center gap-2"
+            onClick={() => setShowSidebar(!showSidebar)}
+          >
+     
+            <FaEnvelope className="text-blue-500" size={20} />
+            <span className="font-medium">Inbox</span>
+          </button>
         </div>
         <div className="flex-1 overflow-auto p-6 space-y-4 bg-gray-50">
           {selectedConversation.messages.map((message, index) => (
-            <div key={index} className={`flex ${message.sender === 'user' ? 'justify-start' : 'justify-end'}`}>
-              <div className={`max-w-[60%] px-4 py-2 rounded-lg text-sm whitespace-pre-line ${message.sender === 'user' ? 'bg-white border border-gray-300' : 'bg-blue-100 text-gray-800'}`}>
+            <div
+              key={index}
+              className={`flex ${
+                message.sender === "user" ? "justify-start" : "justify-end"
+              }`}
+            >
+              <div
+                className={`max-w-[60%] px-4 py-2 rounded-lg text-sm whitespace-pre-line ${
+                  message.sender === "user"
+                    ? "bg-white border border-gray-300"
+                    : "bg-blue-100 text-gray-800"
+                }`}
+              >
                 {message.text}
-                <div className="text-right text-[10px] text-gray-500 mt-1">{message.time}</div>
+                <div className="text-right text-[10px] text-gray-500 mt-1">
+                  {message.time}
+                </div>
               </div>
             </div>
           ))}
--- components/dashboard/createsteps/StepOne.jsx
+++ components/dashboard/createsteps/StepOne.jsx
@@ -4,112 +4,48 @@ import { useState } from "react";
 export default function StepOne({ nextStep, updateFormData }) {
   const [localData, setLocalData] = useState({ title: "", description: "", images: [] });
 
-  // Handle file selection
-  const handleImageUpload = (e) => {
-    const files = Array.from(e.target.files);
-
-    if (localData.images.length + files.length > 5) {
-      alert("You can upload a maximum of 5 images.");
-      return;
-    }
-
-    const newImages = files.map((file) => ({
-      id: URL.createObjectURL(file), // Temporary URL for preview
-      file,
-    }));
-
-    setLocalData((prev) => ({
-      ...prev,
-      images: [...prev.images, ...newImages],
-    }));
-  };
-
-  // Remove an image
-  const handleRemoveImage = (id) => {
-    setLocalData((prev) => ({
-      ...prev,
-      images: prev.images.filter((img) => img.id !== id),
-    }));
-  };
 
   // Handle Next Button Click
   const handleNext = () => {
-    // if (!localData.title.trim() || !localData.description.trim()) {
-    //   alert("Please fill in all fields.");
-    //   return;
-    // }
-
-    // if (localData.images.length < 1) {
-    //   alert("Please add at least one image.");
-    //   return;
-    // }
-
-    updateFormData({
-      title: localData.title,
-      description: localData.description,
-      // images: localData.images.map((img) => img.file), // Store only the file for backend upload
-    });
-
     nextStep();
   };
 
   return (
-    <div className="bg-white max-w-4xl mx-auto">
-      <h2 className="text-xl font-bold mb-4">Step 1: Basic Information</h2>
-
-      {/* Title Input */}
-      <p className="text-gray-700  mb-2">Title</p>
-      <input
-        type="text"
-        placeholder="Car Title"
-        className="border p-2 w-full rounded mb-3"
-        value={localData.title}
-        onChange={(e) => setLocalData({ ...localData, title: e.target.value })}
-      />
-
-      {/* Description Input */}
-      <p className="text-gray-700  mb-2">Description</p>
-      <textarea
-        placeholder="Car Description"
-        className="border p-2 w-full rounded mb-3"
-        value={localData.description}
-        onChange={(e) => setLocalData({ ...localData, description: e.target.value })}
-      ></textarea>
-
-      {/* Image Upload */}
-      <label className="block text-gray-700 mb-2">Upload Images (Max 5)</label>
-      <input
-        type="file"
-        accept="image/*"
-        multiple
-        className="block text-base text-gray-500 file:mr-4 file:py-2 file:px-7 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-gray-700 file:text-white hover:file:bg-gray-600 file:duration-300 border border-gray-300 p-1 w-auto rounded-md"
-        onChange={handleImageUpload}
-      />
-
-      {/* Image Preview */}
-      {/* {localData.images.length > 0 && (
-        <div className="flex flex-wrap gap-2 mt-2">
-          {localData.images.map((img) => (
-            <div key={img.id} className="relative w-24 h-24 border rounded overflow-hidden">
-              <img src={img.id} alt="Uploaded" className="w-full h-full object-cover" />
-              <button
-                className="absolute top-1 right-1 bg-red-500 text-white text-xs px-2 py-1 rounded"
-                onClick={() => handleRemoveImage(img.id)}
-              >
-                ✕
-              </button>
-            </div>
-          ))}
+    <div className="bg-white rounded-lg w-full">
+      <h2 className="text-xl font-bold mb-4">Step 1: Car Details</h2>
+      <div className="grid grid-cols-2 gap-4 w-full">
+        <div className="col-span-2 w-full ">
+          <label className="block text-gray-700 mb-1">Title</label>
+          <input
+            type="text"
+            placeholder="Enter title"
+            className="border p-3 w-full rounded h-12 "
+          />
         </div>
-      )} */}
-
-      {/* Next Button */}
-      <button
-        onClick={handleNext}
-        className="mt-4 bg-blue-500 text-white px-4 py-2 rounded"
-      >
-        Next
-      </button>
+        <div className="col-span-2 w-full ">
+          <label className="block text-gray-700 mb-1">Description</label>
+          <input
+            type="text"
+            placeholder="Enter description"
+            className="border p-3 w-full rounded h-12 "
+          />
+        </div>
+        <div className="col-span-2 w-full ">
+          <label className="block text-gray-700 mb-1">Upload images</label>
+          <input
+            type="file"
+            placeholder="Enter description"
+            className="text-base text-gray-500 file:mr-4 file:py-2 file:px-7 file:rounded-lg file:border-0 file:text-sm file:font-semibold file:bg-gray-700 file:text-white hover:file:bg-gray-600 file:duration-300 border border-gray-300 p-1 w-auto rounded-md"
+            />
+        </div>
+        <div>
+        <button
+          onClick={handleNext}
+          className="bg-blue-500 text-white px-4 py-2 rounded"
+        >
+          Next
+        </button>        </div>
+      </div>
     </div>
   );
 }
