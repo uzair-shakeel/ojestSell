7d424ac01cb76f5851644bfc88c7abb6027b8022
uzair-shakeel <uzairshakeel00000@gmail.com>
2025-04-03 10:16:52 +0500

    public folder updated

--- app/website/layout.jsx
+++ app/website/layout.jsx
@@ -1,18 +1,15 @@
 import Navbar from "../../components/website/Navbar";
-import Footer from "../../components/website/Footer";
+import { Footer } from "../../components/website/footer";
 
 export default function WebsiteLayout({ children }) {
-
   return (
     <div className="flex flex-col min-h-screen h-auto w-full">
-      <Navbar /> 
+      <Navbar />
       {/* Main content adjusts dynamically */}
-      <main
-        className={`flex-1 flex h-auto w-full`}
-      >
+      <main className={`flex-1 flex h-auto w-full`}>
         <div className="w-full">{children}</div>
       </main>
-      <Footer/>
+      <Footer />
     </div>
   );
 }
