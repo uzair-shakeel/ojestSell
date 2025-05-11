b782f5e51e573d31fc737d59c68903d0e14d0852
uzair-shakeel <uzairshakeel00000@gmail.com>
2025-05-12 02:13:51 +0500

    revert

--- app/website/cars/[carId]/page.jsx
+++ app/website/cars/[carId]/page.jsx
@@ -32,7 +32,10 @@ const Page = () => {
 
   const formatImageUrl = (imagePath) => {
     if (!imagePath) return "/images/default-seller.png";
-    return `http://164.90.219.243:8000/${imagePath.replace("\\", "/")}`;
+    return `${process.env.NEXT_PUBLIC_API_BASE_URL}/${imagePath.replace(
+      "\\",
+      "/"
+    )}`;
   };
 
   const getCityFromCoordinates = async (lat, lon) => {
