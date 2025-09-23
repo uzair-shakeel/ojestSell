48e2e356a249ceb5f25d3d63eb429dd27af5a6ba
basitdevs <arsajid090@gmail.com>
2025-09-23 09:18:12 +0500

    adding mutiple images on mobile

--- components/website/CarCard.jsx
+++ components/website/CarCard.jsx
@@ -43,47 +43,113 @@ export default function CarCard({ view, car, onDelete }) {
         view === "list" ? "flex-row" : "flex-col max-w-full"
       }`}
     >
-      <Swiper
-        modules={[Navigation]}
-        navigation={{ prevEl: ".custom-prev", nextEl: ".custom-next" }}
-        className={`${view === "list" ? "w-[700px] h-auto" : "w-full h-[300px]"}`}
-      >
-        {car?.images && car?.images?.length > 0 ? (
-          car?.images?.map((image, index) => {
-            return (
-              <SwiperSlide key={index}>
-                <div className='h-[300px] overflow-hidden rounded-[7px]'>
-                  <Image
-                    src={image}
-                    width={1000}
-                    height={500}
-                    className='w-full h-full object-cover'
-                    alt={`Car Image ${index + 1}`}
-                  />
-                </div>
-              </SwiperSlide>
-            );
-          })
-        ) : (
-          <SwiperSlide>
-            <div className='h-[300px] overflow-hidden rounded-[7px]'>
+      <div className='hidden md:block '>
+        <Swiper
+          modules={[Navigation]}
+          navigation={{ prevEl: ".custom-prev", nextEl: ".custom-next" }}
+          className={`${view === "list" ? "w-[700px] h-auto" : "w-full h-[300px]"}`}
+        >
+          {car?.images && car?.images?.length > 0 ? (
+            car?.images?.map((image, index) => {
+              return (
+                <SwiperSlide key={index}>
+                  <div className='h-[300px] overflow-hidden rounded-[7px]'>
+                    <Image
+                      src={image}
+                      width={1000}
+                      height={500}
+                      className='w-full h-full object-cover'
+                      alt={`Car Image ${index + 1}`}
+                    />
+                  </div>
+                </SwiperSlide>
+              );
+            })
+          ) : (
+            <SwiperSlide>
+              <div className='h-[300px] overflow-hidden rounded-[7px]'>
+                <Image
+                  src='https://via.placeholder.com/500' // Placeholder image
+                  width={1000}
+                  height={500}
+                  className='w-full h-full object-cover'
+                  alt={`Car Image Placeholder`}
+                />
+              </div>
+            </SwiperSlide>
+          )}
+
+          <button className='custom-prev group-hover:opacity-100 opacity-0 transition-all duration-300 absolute left-2 top-1/2 transform -translate-y-1/2 bg-white/30 text-black w-8 h-8 rounded-full shadow-md z-10'>
+            ❮
+          </button>
+          <button className='custom-next group-hover:opacity-100 opacity-0 transition-all duration-300 absolute right-2 top-1/2 transform -translate-y-1/2 bg-white/30 text-black w-8 h-8 rounded-full shadow-md z-10'>
+            ❯
+          </button>
+
+          <div className='flex absolute bottom-1 left-3 z-[10] items-center space-x-2 bg-blue-100 rounded-[4px] h-[24px] pe-3 w-fit my-2'>
+            <div className='bg-blue-400 text-[15px] size-[24px] flex items-center justify-center rounded-l-[4px] text-white'>
+              <FaTags />
+            </div>
+            <span className='text-base text-black'>
+              {car.financialInfo.priceNetto.toLocaleString("pl-PL")} zł
+            </span>
+          </div>
+        </Swiper>
+      </div>
+      
+      <div className='overflow-hidden rounded-[7px] relative md:hidden'>
+        <Image
+          src={
+            car?.images && car?.images.length > 0
+              ? car.images[0]
+              : "https://via.placeholder.com/500"
+          }
+          width={1000}
+          height={500}
+          className='w-full h-[300px] object-cover'
+          alt={`Car Image`}
+        />
+        {car?.images && car?.images.length > 2 && (
+          <div className='grid grid-cols-3 gap-[1px] h-[200px] mt-[1px]'>
+            <div className='col-span-2 h-full'>
               <Image
-                src='https://via.placeholder.com/500' // Placeholder image
+                src={
+                  car?.images && car?.images.length > 0
+                    ? car.images[1]
+                    : "https://via.placeholder.com/500"
+                }
                 width={1000}
                 height={500}
                 className='w-full h-full object-cover'
-                alt={`Car Image Placeholder`}
+                alt={`Car Image`}
               />
             </div>
-          </SwiperSlide>
+            <div className='col-span-1 gap-[1px] h-full'>
+              <Image
+                src={
+                  car?.images && car?.images.length > 0
+                    ? car.images[2]
+                    : "https://via.placeholder.com/500"
+                }
+                width={1000}
+                height={500}
+                className='w-full h-[100px] object-cover'
+                alt={`Car Image`}
+              />
+              <Image
+                src={
+                  car?.images && car?.images.length > 0
+                    ? car.images[3]
+                    : "https://via.placeholder.com/500"
+                }
+                width={1000}
+                height={500}
+                className='w-full h-[100px] mt-[1px] object-cover'
+                alt={`Car Image`}
+              />
+            </div>
+          </div>
         )}
-
-        <button className='custom-prev group-hover:opacity-100 opacity-0 transition-all duration-300 absolute left-2 top-1/2 transform -translate-y-1/2 bg-white/30 text-black w-8 h-8 rounded-full shadow-md z-10'>
-          ❮
-        </button>
-        <button className='custom-next group-hover:opacity-100 opacity-0 transition-all duration-300 absolute right-2 top-1/2 transform -translate-y-1/2 bg-white/30 text-black w-8 h-8 rounded-full shadow-md z-10'>
-          ❯
-        </button>
 
         <div className='flex absolute bottom-1 left-3 z-[10] items-center space-x-2 bg-blue-100 rounded-[4px] h-[24px] pe-3 w-fit my-2'>
           <div className='bg-blue-400 text-[15px] size-[24px] flex items-center justify-center rounded-l-[4px] text-white'>
@@ -93,7 +159,7 @@ export default function CarCard({ view, car, onDelete }) {
             {car.financialInfo.priceNetto.toLocaleString("pl-PL")} zł
           </span>
         </div>
-      </Swiper>
+      </div>
 
       <div
         onClick={() => router.push(`/website/cars/${car._id}`)}
@@ -128,7 +194,7 @@ export default function CarCard({ view, car, onDelete }) {
         {onDelete && (
           <button
             onClick={(e) => {
-              e.stopPropagation(); 
+              e.stopPropagation();
               onDelete(car._id);
             }}
             className='absolute right-4 top-4 rounded-full bg-red-100 p-2 text-red-400 transition-all duration-300 hover:bg-red-300 hover:text-red-700'
